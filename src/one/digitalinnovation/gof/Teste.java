ab118e91b9946cbc6f8f13697b33f079c5c27ab0
Eduardo Rodrigues Santana <eduardo.r.santana@hotmail.com>
2021-10-17 17:33:03 -0300

    Exemplo Design Pattern Strategy

---
+++ src/one/digitalinnovation/gof/Teste.java
@@ -0,0 +1,53 @@
+package one.digitalinnovation.gof;
+
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.Comportamento;
+import one.digitalinnovation.gof.strategy.ComportamentoAgressivo;
+import one.digitalinnovation.gof.strategy.ComportamentoDefensivo;
+import one.digitalinnovation.gof.strategy.ComportamentoNormal;
+import one.digitalinnovation.gof.strategy.Robo;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		//Testes relacionados ao Design Pattern Singleton
+		
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		
+		SingletonLazyHolder holder = SingletonLazyHolder.getInstancia();
+		System.out.println(holder);
+		holder = SingletonLazyHolder.getInstancia();
+		System.out.println(holder);
+		
+		//Testes relacionados ao Design Pattern Strategy
+		
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		robo.setStrategy(normal);
+		robo.mover();
+		robo.mover();
+		robo.setStrategy(defensivo);
+		robo.mover();
+		robo.setStrategy(agressivo);
+		robo.mover();
+		robo.mover();
+		robo.mover();
+
+
+	}
+
+}
